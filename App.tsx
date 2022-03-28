64f302795e77b61337ce97971c436c177a9d4c99
DGUHJH <ghkdwogud852@gmail.com>
2022-03-28 18:21:55 +0900

    Feat : react-Navigation, redux Provider 추가

--- App.tsx
+++ App.tsx
@@ -1,19 +1,24 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import {Provider} from 'react-redux';
+import createStore from './src/features';
 import DetailsScreen from './src/screens/Details';
 import HomeScreen from './src/screens/Home';
 
 const Stack = createNativeStackNavigator();
+const store = createStore();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
