e28aecd2dd1efe7a0235da0c7fd1fa8bcf70cb71
DGUHJH <ghkdwogud852@gmail.com>
2022-03-28 18:21:09 +0900

    Feat : Todo 컴포넌트 활성화

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,24 +1,80 @@
+import CheckBox from '@react-native-community/checkbox';
+import React, {useState} from 'react';
 import {Text, View} from 'react-native';
-import Styles from './styles';
+import EditModal from '../Modal/Edit';
+import styles from './styles';
 
 type Props = {
   id: number;
-  onCheckboxClick?: () => void;
   onUpdateTodoItem?: (value: string) => void;
   onDeleteButtonClick?: () => void;
+  onDetailsButtonClick?: () => void;
   content?: string;
 };
 
 const Todo: React.FC<Props> = ({
   id,
-  onCheckboxClick,
   onDeleteButtonClick,
   onUpdateTodoItem,
+  onDetailsButtonClick,
   content,
 }) => {
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const [checked, setChecked] = useState<boolean>(false);
+
+  const contentLineList = content.split('\n');
+
   return (
-    <View style={Styles.root}>
-      <Text>{content}</Text>
+    <View style={styles.root}>
+      <CheckBox value={checked} onChange={() => setChecked(prev => !prev)} />
+      <View>
+        {contentLineList.map(
+          (contentLine, index) =>
+            index < 5 && (
+              <Text
+                onPress={onDetailsButtonClick}
+                style={{
+                  ...styles.text,
+                  textDecorationLine: checked ? 'line-through' : 'none',
+                }}>
+                {contentLine}
+              </Text>
+            ),
+        )}
+        {contentLineList.length > 5 && (
+          <Text
+            onPress={onDetailsButtonClick}
+            style={{
+              ...styles.text,
+              textDecorationLine: checked ? 'line-through' : 'none',
+            }}>
+            ...
+          </Text>
+        )}
+      </View>
+      <Text
+        onPress={checked ? () => {} : () => setEditMode(prev => !prev)}
+        style={{
+          ...styles.text,
+          textDecorationLine: checked ? 'line-through' : 'none',
+        }}>
+        수정
+      </Text>
+      <Text
+        onPress={checked ? () => {} : onDeleteButtonClick}
+        style={{
+          ...styles.text,
+          textDecorationLine: checked ? 'line-through' : 'none',
+        }}>
+        삭제
+      </Text>
+      {editMode && (
+        <EditModal
+          initialValue={content}
+          onClose={() => setEditMode(false)}
+          onUpdateTodoItem={onUpdateTodoItem}
+        />
+      )}
     </View>
   );
 };
--- src/components/Todo/styles.ts
+++ src/components/Todo/styles.ts
@@ -3,6 +3,11 @@ import {StyleSheet} from 'react-native';
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+    flexDirection: 'row',
+  },
+  text: {
+    marginTop: 7,
+    marginRight: 3,
   },
 });
 
