387c0f8d4b48049a882d28f6168db9cce7a77baf
DGUHJH <ghkdwogud852@gmail.com>
2022-03-28 17:49:03 +0900

    Feat : EditModal 컴포넌트 추가

---
+++ src/components/Modal/Edit/styles.ts
@@ -0,0 +1,32 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  root: {
+    width: '100%',
+    height: 105,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    zIndex: 5,
+  },
+  modalRoot: {
+    width: '80%',
+    backgroundColor: '#fff',
+    flex: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#000',
+  },
+  textInput: {
+    width: 250,
+    height: 100,
+    padding: 10,
+    textAlignVertical: 'top',
+  },
+  text: {marginRight: 3},
+});
+
+export default styles;
