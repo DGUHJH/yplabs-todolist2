ffae08cd82dc5eacb06073548440b093ea2842ac
DGUHJH <ghkdwogud852@gmail.com>
2022-03-30 14:40:54 +0900

    Feat : MarginType 추가

--- src/types/common.ts
+++ src/types/common.ts
@@ -2,3 +2,10 @@ export type RootStackParamList = {
   Home: undefined;
   Details: {id: number};
 };
+
+export type MarginType = {
+  marginTop?: number;
+  marginBottom?: number;
+  marginLeft?: number;
+  marginRight?: number;
+};
