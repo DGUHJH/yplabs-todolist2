87000ae64940ad8b5cd5d678158b3fe438a8912c
DGUHJH <ghkdwogud852@gmail.com>
2022-03-30 14:56:56 +0900

    Fix : CommonImage 컴포넌트 onPress 안되던 오류 수정

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,7 +1,7 @@
 import styles from './styles';
 import React from 'react';
 import {MarginType} from '../../types/common';
-import {Image, ImageSourcePropType} from 'react-native';
+import {Image, ImageSourcePropType, TouchableOpacity} from 'react-native';
 
 type Props = {
   source: ImageSourcePropType;
@@ -11,16 +11,18 @@ type Props = {
 } & MarginType;
 
 const CommonImage: React.FC<Props> = props => (
-  <Image
-    {...props}
-    style={{
-      width: props.size,
-      height: props.size,
-      marginTop: props.marginTop ?? 0,
-      marginBottom: props.marginBottom ?? 0,
-      marginLeft: props.marginLeft ?? 0,
-      marginRight: props.marginRight ?? 0,
-    }}
-  />
+  <TouchableOpacity onPress={props.onPress}>
+    <Image
+      {...props}
+      style={{
+        width: props.size,
+        height: props.size,
+        marginTop: props.marginTop ?? 0,
+        marginBottom: props.marginBottom ?? 0,
+        marginLeft: props.marginLeft ?? 0,
+        marginRight: props.marginRight ?? 0,
+      }}
+    />
+  </TouchableOpacity>
 );
 export default CommonImage;
