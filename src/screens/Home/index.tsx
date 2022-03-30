df2828d2584b586f1a1a3282dbc7a5cdc32cdd23
DGUHJH <ghkdwogud852@gmail.com>
2022-03-30 15:33:32 +0900

    Feat : Todolist Infinite scroll 구현

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,13 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import React, {useEffect, useState} from 'react';
-import {FlatList, RefreshControl, ScrollView, Text, View} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {
+  FlatList,
+  RefreshControl,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useDispatch, useSelector} from 'react-redux';
 import CommonModal from '../../components/Modal';
@@ -10,12 +17,14 @@ import {refreshTodoListLoad, toggleModal} from '../../features/todo/slice';
 import useAsyncRefresh from '../../hooks/useAsyncRefresh';
 import {RootStackParamList} from '../../types/common';
 import styles from './styles';
+import {debounce} from 'lodash';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 const HomeScreen: React.FC<Props> = ({navigation}) => {
   const store = useSelector((state: RootState) => state);
   const dispatch = useDispatch();
   const [refreshing, onRefresh] = useAsyncRefresh();
+  const [scroll, setScroll] = useState<number>(0);
 
   useEffect(() => {
     refreshTodoList();
@@ -28,6 +37,12 @@ const HomeScreen: React.FC<Props> = ({navigation}) => {
     dispatch(toggleModal({type: 'create', open: true}));
   };
 
+  const onEndReached = useCallback(
+    debounce(() => {
+      setScroll(prev => prev + 1);
+    }, 500),
+    [scroll],
+  );
   return (
     <SafeAreaView style={styles.root}>
       <FlatList
@@ -36,20 +51,25 @@ const HomeScreen: React.FC<Props> = ({navigation}) => {
         refreshControl={
           <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
-        renderItem={({item}) => (
-          <Todo
-            id={item.id}
-            content={item.content}
-            onDetailsButtonClick={() =>
-              navigation.push('Details', {id: item.id})
-            }
-            key={`todo_item_${item.id}`}
-          />
-        )}
+        renderItem={({item, index}) =>
+          index < 18 + scroll * 2 && (
+            <Todo
+              id={item.id}
+              content={item.content}
+              onDetailsButtonClick={() =>
+                navigation.push('Details', {id: item.id})
+              }
+              key={`todo_item_${item.id}`}
+            />
+          )
+        }
+        onEndReached={onEndReached}
       />
-      <View style={styles.addButtonWrapper}>
-        <Text onPress={onAddButtonClick}>추가</Text>
-      </View>
+      <TouchableOpacity
+        onPress={onAddButtonClick}
+        style={styles.addButtonWrapper}>
+        <Text>추가</Text>
+      </TouchableOpacity>
       <CommonModal />
     </SafeAreaView>
   );
