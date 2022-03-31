4befcf55ca5396b82de373d040093ed7ff5e5178
DGUHJH <ghkdwogud852@gmail.com>
2022-03-31 10:38:23 +0900

    Feat : Typography props 추가

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,15 +1,49 @@
 import {Text, View} from 'react-native';
 import Styles from './styles';
+import colors from '../../styles/colors';
+import {MarginType} from '../../types/common';
 
 type Props = {
   color: string;
   fontSize: number;
-};
+  fontWeight:
+    | 'normal'
+    | 'bold'
+    | '100'
+    | '200'
+    | '300'
+    | '400'
+    | '500'
+    | '600'
+    | '700'
+    | '800'
+    | '900';
+} & MarginType;
 
-const Typography: React.FC<Props> = ({color, fontSize, children}) => {
+const Typography: React.FC<Props> = ({
+  color,
+  fontSize,
+  fontWeight,
+  marginTop,
+  marginBottom,
+  marginLeft,
+  marginRight,
+  children,
+}) => {
   return (
     <View style={Styles.root}>
-      <Text style={{color, fontSize}}>{children}</Text>
+      <Text
+        style={{
+          color,
+          fontSize,
+          fontWeight,
+          marginTop,
+          marginBottom,
+          marginLeft,
+          marginRight,
+        }}>
+        {children}
+      </Text>
     </View>
   );
 };
